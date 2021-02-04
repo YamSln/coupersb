a9bf2c4acd2090ccfea5229be925b76b8dea2e72
Jamonse <yam467@gmail.com>
2021-02-04 11:39:15 +0200

    Heroku deployment

--- src/main/java/local/coupersb/controller/WebConfig.java
+++ src/main/java/local/coupersb/controller/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer
     public void addCorsMappings(CorsRegistry registry) 
     {
         registry.addMapping("/**")
-        	.allowedOrigins("http://localhost:4200")
+        	.allowedOrigins("${spa-app.url}")
         	.allowedMethods("*")
         	.allowCredentials(true);
     }
